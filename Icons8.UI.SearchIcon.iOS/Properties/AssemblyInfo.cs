7139d82c22c000cbcffeb0e90c7d34c891868001
Horev Ivan <ivanhorev@icons8.com>
2017-03-13 02:43:24 +0700

    created

---
+++ Icons8.UI.SearchIcon.iOS/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("Icons8.UI.SearchIcon.iOS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(I8_Assembly.Company)]
+[assembly: AssemblyProduct(I8_Assembly.Product)]
+[assembly: AssemblyCopyright(I8_Assembly.Copyright)]
+[assembly: AssemblyTrademark(I8_Assembly.Trademark)]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+
+[assembly: ComVisible(false)]
