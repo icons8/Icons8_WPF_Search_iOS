45a1fe2a7c72a3c8b9a77b6489e62569f671ba28
Horev Ivan <ivanhorev@icons8.com>
2017-03-13 12:05:01 +0700

    v0.1.1 fix control

--- Icons8.UI.SearchIcon.iOS/Properties/AssemblyInfo.cs
+++ Icons8.UI.SearchIcon.iOS/Properties/AssemblyInfo.cs
@@ -13,6 +13,6 @@ using System.Windows;
 [assembly: AssemblyTrademark(I8_Assembly.Trademark)]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
 
 [assembly: ComVisible(false)]
